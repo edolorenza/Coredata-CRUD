798e3b76331e3807ce9d562b1cf7648efabd1ad7
edo Lorenza <edolrnz@gmail.com>
2021-06-23 11:08:32 +0700

    Initial Commit

---
+++ CoreData-CRUD/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreData-CRUD
+//
+//  Created by Edo Lorenza on 23/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
